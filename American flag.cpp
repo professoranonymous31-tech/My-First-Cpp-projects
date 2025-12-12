ee31fa0c3eefd316d1fe11dd53cb2e04ee75e607
professoranonymous31-tech <professoranonymous31@gmail.com>
2025-12-12 08:41:30 +0500

    Update American flag.cpp

--- American flag.cpp
+++ American flag.cpp
@@ -2,26 +2,26 @@
 using namespace std;
 
 int main() {
-    // Flag dimensions
-    int starRows = 9;      // Rows containing stars
-    int starCols = 12;     // Width of the star field
-    int totalRows = 15;    // Total rows of the flag
-    int totalCols = 40;    // Width of the flag
+    
+    int starRows = 9;     
+    int starCols = 12;     
+    int totalRows = 15;    
+    int totalCols = 40;    
 
     for (int i = 0; i < totalRows; i++) {
-        // Rows with the blue field + stars
+       
         if (i < starRows) {
-            // Print star field+
+
             for (int j = 0; j < starCols; j++) {
                 if (j % 2 == 0) cout << "*";
                 else cout << " ";
             }
-            // Print rest of the stripe
+    
             for (int j = starCols; j < totalCols; j++) {
                 cout << "=";
             }
         } 
-        // Rows without star field
+     
         else {
             for (int j = 0; j < totalCols; j++) {
                 cout << "=";
@@ -32,3 +32,4 @@ int main() {
     system("pause");
     return 0;
 }
+
